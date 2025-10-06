bbe258326b1ffef56d9d898d65605fd4dd819e96
billayamini18 <billayamini18@gmail.com>
2025-10-06 21:45:19 +0530

    auth.py

--- auth.py
+++ auth.py
@@ -1,12 +1,9 @@
 from data import accounts
 def authenticate_user(name, pin):
-    """
-    Finds and authenticates a user based on name and PIN.
-    Returns the user's account dictionary if successful, otherwise None.
-    """
     for account in accounts:
         # if both and pin is correct 
         if account["name"].lower() == name.lower() and account["pin"] == pin:
             return account
 
     return None
+
